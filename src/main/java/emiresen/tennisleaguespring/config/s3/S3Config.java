b5383d21db6d91b12311c9b6a9c48cabac38d597
emiresen <emiresen246@gmail.com>
2024-08-22 23:28:43 +0300

    Cloud config added .yml and classes

--- src/main/java/emiresen/tennisleaguespring/config/s3/S3Config.java
+++ src/main/java/emiresen/tennisleaguespring/config/s3/S3Config.java
@@ -3,8 +3,10 @@ package emiresen.tennisleaguespring.config.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -12,23 +14,25 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    //    using .aws
-    @Value("${aws.credentials.profile}")
-    private String awsProfile;
+    @Value("${aws.credentials.accessKeyId}")
+    private String awsAccessKeyId;
+
+    @Value("${aws.credentials.secretAccessKey}")
+    private String awsSecretAccessKey;
 
     @Value("${aws.region}")
     private String region;
-    private AwsCredentialsProvider credentialsProvider(String profileName) {
-        return ProfileCredentialsProvider.builder()
-                .profileName(profileName)
-                .build();
-    }
+
 
     @Bean
     public S3Client s3Client(){
+        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey)
+        );
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(credentialsProvider(awsProfile))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
