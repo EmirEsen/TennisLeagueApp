64bd4c47a3634962af762dd02ec7bc11af07aad7
emiresen <emiresen246@gmail.com>
2024-08-30 21:06:21 +0300

    cors config domain names added

--- src/main/java/emiresen/tennisleaguespring/config/WebConfig.java
+++ src/main/java/emiresen/tennisleaguespring/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://34.45.161.246")
+                .allowedOrigins("http://localhost:5173", "http://34.45.161.246", "https://tennisclub.biz", "https://www.tennisclub.biz" )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
