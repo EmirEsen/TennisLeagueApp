65a89c15b82d9c2c859ef710f9d6f6c141241d56
emiresen <emiresen246@gmail.com>
2024-09-01 18:24:23 +0300

    mongo atlas config added

---
+++ src/main/java/emiresen/tennisleaguespring/config/MongoConfig.java
@@ -0,0 +1,45 @@
+package emiresen.tennisleaguespring.config;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "admin"; // Your default database name
+    }
+
+    @Bean
+    @Override
+    public MongoClient mongoClient() {
+        String connectionString = "mongodb+srv://" +
+                "emiresen" + ":" +
+                "1234" + "@" +
+                "tennisapp.1hde0.mongodb.net/?retryWrites=true&w=majority&appName=tennisapp";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
+    }
+}
