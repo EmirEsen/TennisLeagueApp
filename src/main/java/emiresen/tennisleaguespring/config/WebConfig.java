570cbffd8d863a4a6b23a847bbd0c72f16ca0c4c
emiresen <emiresen246@gmail.com>
2024-09-03 23:39:57 +0300

    webconfig cors changes

--- src/main/java/emiresen/tennisleaguespring/config/WebConfig.java
+++ src/main/java/emiresen/tennisleaguespring/config/WebConfig.java
@@ -1,4 +1,5 @@
 package emiresen.tennisleaguespring.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://34.45.161.246", "https://tennisclub.biz", "https://www.tennisclub.biz" )
+                .allowedOrigins("https://tennisclub.biz",
+                        "https://www.tennisclub.biz")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
