31bd0add78a5e117636e0fbd49538e6c2257e77c
emiresen <emiresen246@gmail.com>
2024-09-13 16:18:55 +0300

    removed unused import

--- src/main/java/emiresen/tennisleaguespring/service/EmailService.java
+++ src/main/java/emiresen/tennisleaguespring/service/EmailService.java
@@ -9,7 +9,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Map;
 
